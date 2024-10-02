e0f10c6eccc6ff6c81cf1fed7e655bad32926c19
sanyalgopl <sanyal@gordinateur.com>
2024-10-02 17:37:44 +0530

    services page desktop view update

--- services.php
+++ services.php
@@ -133,8 +133,184 @@
           </div>
         </div>
       </div>
-    </div>
-  </section> -->
+      </div>
+    </section> -->
+    <section class="page-container">
+      <div class="container-fluid">
+        <div class="row flex-nowrap vh-100">
+          <div class="sidebar" id="sidebar">
+            <a href="/">
+              <img src="images/arrow_circle_left.svg" class="img-fluid" alt="back arrow">
+              Services
+            </a>
+          </div>
+          <div class="service-list bg-accentblue" id="services">
+            <div class="nav-box">
+              <ul class="nav nav-tabs d-none d-md-block border-0" id="myTab" role="tablist">
+                <li class="nav-item" role="presentation">
+                  <a class="nav-link active f-semibold" data-bs-toggle="tab" href="#road-transportation" role="tab">Road Transportation <img src="images/service_arrow_pink.svg" class="img-fluid" alt="arrow icon"></a>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <a class="nav-link f-semibold" data-bs-toggle="tab" href="#logistics-consultancy" role="tab" >Logistics Consultancy <img src="images/service_arrow_pink.svg" class="img-fluid" alt="arrow icon"></a>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <a class="nav-link f-semibold" data-bs-toggle="tab" href="#man-power-supply" role="tab">Man Power Supply <img src="images/service_arrow_pink.svg" class="img-fluid" alt="arrow icon"></a>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <a class="nav-link f-semibold" data-bs-toggle="tab" href="#import-export" role="tab">Import & Export <img src="images/service_arrow_pink.svg" class="img-fluid" alt="arrow icon"></a>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <a class="nav-link f-semibold" data-bs-toggle="tab" href="#infra-projects" role="tab">Infra Projects - Transportation & Consultancy <img src="images/service_arrow_pink.svg" class="img-fluid" alt="arrow icon"></a>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <a class="nav-link f-semibold" data-bs-toggle="tab" href="#cfs" role="tab">Container freight station (CFS) <img src="images/service_arrow_pink.svg" class="img-fluid" alt="arrow icon"></a>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <a class="nav-link f-semibold" data-bs-toggle="tab" href="#logistic-management" role="tab">Logistic Management <img src="images/service_arrow_pink.svg" class="img-fluid" alt="arrow icon"></a>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <a class="nav-link f-semibold" data-bs-toggle="tab" href="#port-handling" role="tab">Port Handling <img src="images/service_arrow_pink.svg" class="img-fluid" alt="arrow icon"></a>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <a class="nav-link f-semibold" data-bs-toggle="tab" href="#nvocc" role="tab">NVOCC <img src="images/service_arrow_pink.svg" class="img-fluid" alt="arrow icon"></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="service-details blue-mid-line bg-accentblue">
+            <div class="tab-content accordion bg-accentblue" id="myTabContent">
+              <div class="tab-pane show active accordion-item" id="road-transportation" role="tabpanel">
+                <h2 class="accordion-header d-lg-none" id="headingOne">
+                  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#road-transportation-tab" aria-expanded="true" aria-controls="collapseOne">Road Transportation</button>
+                </h2>
+                <div id="road-transportation-tab" class="accordion-collapse collapse show d-lg-block" aria-labelledby="headingOne" data-bs-parent="#myTabContent">
+                  <div class="accordion-body p-0 text-center">
+                    <div class="img-box">
+                      <img src="images/home-road-transport.svg" class="img-fluid" alt="Road Transportation">
+                    </div>
+                    <h4 class="f-semibold">Road Transportation</h4>
+                    <p class="f-regular">We handle your transports via road from point A to point B. As a National service provider, we maintain an extensive network structure on various parts of India for road freight transports. With years of experience in the Industry, we have developed the best Road Transportation service for our customers. We focus on timely delivery of goods as per customer needs and committed to providing complete satisfaction.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="tab-pane accordion-item" id="logistics-consultancy" role="tabpanel">
+                <h2 class="accordion-header d-lg-none" id="headingTwo">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#logistics-consultancy-tab" aria-expanded="false" aria-controls="collapseTwo">Logistics Consultancy</button>
+                </h2>
+                <div id="logistics-consultancy-tab" class="accordion-collapse collapse show d-lg-block" aria-labelledby="headingTwo" data-bs-parent="#myTabContent">
+                  <div class="accordion-body p-0 text-center">
+                    <div class="img-box">
+                      <img src="images/home-road-transport.svg" class="img-fluid" alt="Logistics Consultancy">
+                    </div>
+                    <h4 class="f-semibold">Logistics Consultancy</h4>
+                    <p class="f-regular">We handle your transports via road from point A to point B. As a National service provider, we maintain an extensive network structure on various parts of India for road freight transports. With years of experience in the Industry, we have developed the best Road Transportation service for our customers. We focus on timely delivery of goods as per customer needs and committed to providing complete satisfaction.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="tab-pane accordion-item" id="man-power-supply" role="tabpanel">
+                <h2 class="accordion-header d-lg-none" id="headingThree">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#man-power-supply-tab" aria-expanded="false" aria-controls="collapseThree">Man Power Supply</button>
+                </h2>
+                <div id="man-power-supply-tab" class="accordion-collapse collapse show d-lg-block" aria-labelledby="headingThree" data-bs-parent="#myTabContent">
+                  <div class="accordion-body p-0 text-center">
+                    <div class="img-box">
+                      <img src="images/home-road-transport.svg" class="img-fluid" alt="Man Power Supply">
+                    </div>
+                    <h4 class="f-semibold">Man Power Supply</h4>
+                    <p class="f-regular">We handle your transports via road from point A to point B. As a National service provider, we maintain an extensive network structure on various parts of India for road freight transports. With years of experience in the Industry, we have developed the best Road Transportation service for our customers. We focus on timely delivery of goods as per customer needs and committed to providing complete satisfaction.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="tab-pane accordion-item" id="import-export" role="tabpanel">
+                <h2 class="accordion-header d-lg-none" id="headingFour">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#import-export-tab" aria-expanded="false" aria-controls="collapseFour">Import & Export</button>
+                </h2>
+                <div id="import-export-tab" class="accordion-collapse collapse show d-lg-block" aria-labelledby="headingFour" data-bs-parent="#myTabContent">
+                  <div class="accordion-body p-0 text-center">
+                    <div class="img-box">
+                      <img src="images/home-road-transport.svg" class="img-fluid" alt="Import & Export">
+                    </div>
+                    <h4 class="f-semibold">Import & Export</h4>
+                    <p class="f-regular">We handle your transports via road from point A to point B. As a National service provider, we maintain an extensive network structure on various parts of India for road freight transports. With years of experience in the Industry, we have developed the best Road Transportation service for our customers. We focus on timely delivery of goods as per customer needs and committed to providing complete satisfaction.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="tab-pane accordion-item" id="infra-projects" role="tabpanel">
+                <h2 class="accordion-header d-lg-none" id="headingFive">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#infra-projects-tab" aria-expanded="false" aria-controls="collapseFive">Infra Projects - Transportation & Consultancy</button>
+                </h2>
+                <div id="infra-projects-tab" class="accordion-collapse collapse show d-lg-block" aria-labelledby="headingFive" data-bs-parent="#myTabContent">
+                  <div class="accordion-body p-0 text-center">
+                    <div class="img-box">
+                      <img src="images/infra-projects.svg" class="img-fluid" alt="Infra Projects - Transportation & Consultancy">
+                    </div>
+                    <h4 class="f-semibold">Infra Projects - Transportation & Consultancy</h4>
+                    <p class="f-regular">We handle your transports via road from point A to point B. As a National service provider, we maintain an extensive network structure on various parts of India for road freight transports. With years of experience in the Industry, we have developed the best Road Transportation service for our customers. We focus on timely delivery of goods as per customer needs and committed to providing complete satisfaction.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="tab-pane accordion-item" id="cfs" role="tabpanel">
+                <h2 class="accordion-header d-lg-none" id="headingSix">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#cfs-tab" aria-expanded="false" aria-controls="collapseSix">Container freight station (CFS)</button>
+                </h2>
+                <div id="cfs-tab" class="accordion-collapse collapse show d-lg-block" aria-labelledby="headingSix" data-bs-parent="#myTabContent">
+                  <div class="accordion-body p-0 text-center">
+                    <div class="img-box">
+                      <img src="images/home-road-transport.svg" class="img-fluid" alt="Container freight station (CFS)">
+                    </div>
+                    <h4 class="f-semibold">Container freight station (CFS)</h4>
+                    <p class="f-regular">We handle your transports via road from point A to point B. As a National service provider, we maintain an extensive network structure on various parts of India for road freight transports. With years of experience in the Industry, we have developed the best Road Transportation service for our customers. We focus on timely delivery of goods as per customer needs and committed to providing complete satisfaction.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="tab-pane accordion-item" id="logistic-management" role="tabpanel">
+                <h2 class="accordion-header d-lg-none" id="headingSeven">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#logistic-management-tab" aria-expanded="false" aria-controls="collapseSeven">Logistic Management</button>
+                </h2>
+                <div id="logistic-management-tab" class="accordion-collapse collapse show d-lg-block" aria-labelledby="headingSeven" data-bs-parent="#myTabContent">
+                  <div class="accordion-body p-0 text-center">
+                    <div class="img-box">
+                      <img src="images/logistic-management.svg" class="img-fluid" alt="Logistic Management">
+                    </div>
+                    <h4 class="f-semibold">Logistic Management</h4>
+                    <p class="f-regular">We handle your transports via road from point A to point B. As a National service provider, we maintain an extensive network structure on various parts of India for road freight transports. With years of experience in the Industry, we have developed the best Road Transportation service for our customers. We focus on timely delivery of goods as per customer needs and committed to providing complete satisfaction.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="tab-pane accordion-item" id="port-handling" role="tabpanel">
+                <h2 class="accordion-header d-lg-none" id="headingEight">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#port-handling-tab" aria-expanded="false" aria-controls="collapseEight">Port Handling</button>
+                </h2>
+                <div id="port-handling-tab" class="accordion-collapse collapse show d-lg-block" aria-labelledby="headingEight" data-bs-parent="#myTabContent">
+                  <div class="accordion-body p-0 text-center">
+                    <div class="img-box">
+                      <img src="images/home-road-transport.svg" class="img-fluid" alt="Port Handling">
+                    </div>
+                    <h4 class="f-semibold">Port Handling</h4>
+                    <p class="f-regular">We handle your transports via road from point A to point B. As a National service provider, we maintain an extensive network structure on various parts of India for road freight transports. With years of experience in the Industry, we have developed the best Road Transportation service for our customers. We focus on timely delivery of goods as per customer needs and committed to providing complete satisfaction.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="tab-pane accordion-item" id="nvocc" role="tabpanel">
+                <h2 class="accordion-header d-lg-none" id="headingNine">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#nvocc-tab" aria-expanded="false" aria-controls="collapseNine">NVOCC</button>
+                </h2>
+                <div id="nvocc-tab" class="accordion-collapse collapse show d-lg-block" aria-labelledby="headingNine" data-bs-parent="#myTabContent">
+                  <div class="accordion-body p-0 text-center">
+                    <div class="img-box">
+                      <img src="images/nvocc.svg" class="img-fluid" alt="NVOCC">
+                    </div>
+                    <h4 class="f-semibold">NVOCC</h4>
+                    <p class="f-regular">We handle your transports via road from point A to point B. As a National service provider, we maintain an extensive network structure on various parts of India for road freight transports. With years of experience in the Industry, we have developed the best Road Transportation service for our customers. We focus on timely delivery of goods as per customer needs and committed to providing complete satisfaction.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
   <?php include("footer.php"); ?>
   <script type="text/javascript">
     // $(document).ready(function(){
@@ -154,11 +330,7 @@
       var ele_top=$('.tab-content');
       var sect = $(this).attr('href');
       console.log(sect);
-      if(sect == '#recruitment') {
-        $('.service-form').hide();
-      } else {
-        $('.service-form').show();
-      }
+
       var hash=window.location.hash;
       var ele=$('.nav-item a[href="'+hash+'"][data-bs-toggle="tab"]');
       var ele_top=$('.tab-content');
@@ -186,11 +358,6 @@
       var hash=window.location.hash;
       var ele=$('.nav-item a[href="'+hash+'"][data-bs-toggle="tab"]');
       var ele_top=$('.tab-content');
-      if(hash == '#recruitment') {
-        $('.service-form').hide();
-      } else {
-        $('.service-form').show();
-      }
       if(ele.length > 0) {
         $('.nav-item .nav-link').removeClass('active');
         $('.nav-item .nav-link').attr('aria-selected','false');
@@ -210,11 +377,6 @@
           var hash = window.location.hash;
           var ele = $('.nav-item a[href="'+hash+'"][data-bs-toggle="tab"]');
           var ele_top = $('.tab-content');
-          if(hash == '#recruitment') {
-            $('.service-form').hide();
-          } else {
-            $('.service-form').show();
-          }
           if(ele.length > 0) {
             $('.nav-item .nav-link').removeClass('active');
             $('.nav-item .nav-link').attr('aria-selected','false');
@@ -231,11 +393,6 @@
         var index = url.indexOf("#");
         var hash = url.substring(index + 1);
         $(function(){
-          if(hash == 'recruitment') {
-            $('.service-form').hide();
-          } else {
-            $('.service-form').show();
-          }
           // var hash = window.location.hash;
           // console.log(hash);
           if(index > -1) {
@@ -255,11 +412,6 @@
         $('.accordion-button').on('click', function(e) {
           var sects = $(this).parent().parent().attr('id');
           console.log(sects);
-          if(sects == 'recruitment') {
-            $('.service-form').hide();
-          } else {
-            $('.service-form').show();
-          }
           var elet = $('.tab-pane#'+hash);
           $('.tab-pane').removeClass('active show');
           $('.tab-pane button').attr('aria-expanded','false');
